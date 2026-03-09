87df629fada4e932cc296476c3d15824d52c7262
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2026-03-09 15:48:36 +0100

    added cloner, and available encodings for completions

--- unzipper-lib/src/lib.rs
+++ unzipper-lib/src/lib.rs
@@ -1,14 +1,42 @@
 //! Library used to unzip zip content.
 
+mod cloner;
 mod encoding;
 
-use ::core::num::NonZero;
-use ::std::{fs::File, io::BufReader, path::Path, sync::Arc};
+use ::core::{
+    iter,
+    num::NonZero,
+    sync::atomic::{self, AtomicUsize},
+};
+use ::std::{
+    fs::File,
+    io::BufReader,
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
+use ::chardetng::EncodingDetector;
 use ::parking_lot::Mutex;
+use ::rayon::iter::{
+    IntoParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator,
+};
+use ::tap::Conv;
 use ::zip::ZipArchive;
 
+use crate::cloner::fallible_cloner;
+
 pub use self::encoding::{Encoding, EncodingFromStrError};
+
+/// Errors returned by attempts to unzip files.
+#[derive(Debug, ::thiserror::Error)]
+pub enum UnzipError {
+    /// Error returned when source file could not be opened.
+    #[error("could not open file\n{0}")]
+    OpenSrc(::std::io::Error),
+    /// Error returned when source file is not a zip archive.
+    #[error("file is not a zip archive\n{0}")]
+    ReadZip(::zip::result::ZipError),
+}
 
 /// Unzipper configuration.
 #[derive(Debug, Clone)]
@@ -19,6 +47,8 @@ pub struct Unzipper {
     pub threads: NonZero<usize>,
     /// Should common path prefix be removed.
     pub unfold: bool,
+    /// Password to use for files.
+    pub password: Vec<u8>,
 }
 
 impl Unzipper {
@@ -28,41 +58,104 @@ impl Unzipper {
     /// On io errors related to reading src.
     /// Or if src is not a zip file / is malformed.
     /// Or if dest cannot be created / cannot be written to.
-    pub fn unzip(&self, src: &Path, dest: &Path) -> ::std::io::Result<()> {
+    pub fn unzip(&self, src: &Path, dest: &Path) -> Result<(), UnzipError> {
         let Self {
             encoding,
             threads,
             unfold,
+            password,
         } = self;
 
-        let file = File::open(src)?;
-        let archive = ZipArchive::new(BufReader::new(&file))?;
-        let metadata = archive.metadata();
+        let file = File::open(src).map_err(UnzipError::OpenSrc)?;
 
-        let thread_archives = (0..threads.get())
-            .filter_map(|_| {
+        let mut metadata = None;
+        let mut len = 0;
+        let mut archives = fallible_cloner(file, *threads, File::try_clone)
+            .filter_map(|file| {
                 let file = file
-                    .try_clone()
-                    .inspect_err(|err| {
-                        ::log::warn!("failed to clone file handle of {src:?}\n{err}")
-                    })
+                    .map_err(|err| ::log::warn!("failed to clone file handle of {src:?}\n{err}"))
                     .ok()
                     .map(BufReader::new)?;
-                let meta = Arc::clone(&metadata);
 
-                // SAFETY: Same file as metadata was created for is used.
-                let archive = unsafe { ZipArchive::unsafe_new_with_metadata(file, meta) };
-                Some(archive)
+                if let Some(metadata) = &metadata {
+                    let metadata = Arc::clone(metadata);
+                    // SAFETY: Same file as metadata was created for is used.
+                    let archive = unsafe { ZipArchive::unsafe_new_with_metadata(file, metadata) };
+                    Some(Ok(archive))
+                } else {
+                    match ZipArchive::new(file) {
+                        Ok(archive) => {
+                            metadata = Some(archive.metadata());
+                            len = archive.len();
+                            Some(Ok(archive))
+                        }
+                        Err(err) => Some(Err(err)),
+                    }
+                }
+            })
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(UnzipError::ReadZip)?;
+        let _ = metadata;
+        let len = len;
+
+        let counter = AtomicUsize::new(0);
+        let items = archives
+            .par_iter_mut()
+            .flat_map_iter(|archive| {
+                iter::from_fn(|| {
+                    let idx = counter.fetch_add(1, atomic::Ordering::Relaxed);
+                    if idx < len { Some(idx) } else { None }
+                })
+                .fuse()
+                .filter_map(|index| {
+                    let name = archive
+                        .by_index_raw(index)
+                        .map_err(|err| {
+                            ::log::error!("could not get file with index {index} in {src:?}\n{err}")
+                        })
+                        .ok()?
+                        .name_raw()
+                        .conv::<Vec<u8>>();
+
+                    Some((index, name))
+                })
             })
             .collect::<Vec<_>>();
 
+        let encoding = match encoding {
+            Encoding::Auto => {
+                let mut detector = EncodingDetector::new();
+                for (_, name) in &items {
+                    detector.feed(name, false);
+                }
+                detector.feed(b"", true);
+                let (encoding, confident) = detector.guess_assess(None, true);
+                if confident {
+                    ::log::info!("using encoding with confidence, {}", encoding.name());
+                    encoding
+                } else {
+                    ::log::info!("no encoding confidence, using utf-8");
+                    ::encoding_rs::UTF_8
+                }
+            }
+            Encoding::Set(encoding) => {
+                ::log::info!("using set encoding, {}", encoding.name());
+                encoding
+            }
+        };
+
+        let stack = Mutex::new(items);
+        archives.into_par_iter().for_each(|mut archive| {
+            loop {
+                let item = stack.lock().pop();
+                let Some((index, name)) = item else {
+                    break;
+                };
+
+                let (decoded, has_replaced) = encoding.decode_without_bom_handling(&name);
+            }
+        });
+
         Ok(())
     }
-}
-
-/// Archive item.
-#[derive(Debug)]
-struct Item {
-    /// Index of archive item.
-    index: usize,
 }
--- unzipper/src/main.rs
+++ unzipper/src/main.rs
@@ -1,7 +1,74 @@
 //! Implementation of cli tool.
 
+use ::std::io::{self, Write};
+
 use ::clap::Parser;
 use ::log::LevelFilter;
+use ::mimalloc::MiMalloc;
+
+use crate::encoding::ENCODING_NAMES;
+
+/// Global allocator is mimalloc
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+mod encoding {
+    //! Encoding cli help.
+
+    use ::std::{collections::BTreeSet, sync::LazyLock};
+
+    use ::encoding_rs::{
+        BIG5, EUC_JP, EUC_KR, GB18030, GBK, IBM866, ISO_2022_JP, ISO_8859_2, ISO_8859_3,
+        ISO_8859_4, ISO_8859_5, ISO_8859_6, ISO_8859_7, ISO_8859_8, ISO_8859_8_I, ISO_8859_10,
+        ISO_8859_13, ISO_8859_14, ISO_8859_15, ISO_8859_16, KOI8_R, KOI8_U, MACINTOSH, SHIFT_JIS,
+        UTF_8, WINDOWS_874, WINDOWS_1250, WINDOWS_1251, WINDOWS_1252, WINDOWS_1253, WINDOWS_1254,
+        WINDOWS_1255, WINDOWS_1256, WINDOWS_1257, WINDOWS_1258, X_MAC_CYRILLIC,
+    };
+
+    /// Encodings used for command completion.
+    static ENCODINGS: &[&::encoding_rs::Encoding] = &[
+        BIG5,
+        EUC_JP,
+        EUC_KR,
+        GB18030,
+        GBK,
+        IBM866,
+        ISO_2022_JP,
+        ISO_8859_2,
+        ISO_8859_3,
+        ISO_8859_4,
+        ISO_8859_5,
+        ISO_8859_6,
+        ISO_8859_7,
+        ISO_8859_8,
+        ISO_8859_8_I,
+        ISO_8859_10,
+        ISO_8859_13,
+        ISO_8859_14,
+        ISO_8859_15,
+        ISO_8859_16,
+        KOI8_R,
+        KOI8_U,
+        MACINTOSH,
+        SHIFT_JIS,
+        UTF_8,
+        WINDOWS_874,
+        WINDOWS_1250,
+        WINDOWS_1251,
+        WINDOWS_1252,
+        WINDOWS_1253,
+        WINDOWS_1254,
+        WINDOWS_1255,
+        WINDOWS_1256,
+        WINDOWS_1257,
+        WINDOWS_1258,
+        X_MAC_CYRILLIC,
+    ];
+
+    /// Names of available encodings.
+    pub static ENCODING_NAMES: LazyLock<BTreeSet<&'static str>> =
+        LazyLock::new(|| ENCODINGS.iter().map(|enc| enc.name()).collect());
+}
 
 /// Unzip files, with encoding detection.
 #[derive(Debug, Clone, Parser)]
@@ -25,6 +92,14 @@ fn main() -> ::color_eyre::Result<()> {
             },
         )
         .init();
+
+    let mut stdout = io::stdout().lock();
+    for i in ENCODING_NAMES.iter() {
+        stdout
+            .write_all(i.as_bytes())
+            .and_then(|_| stdout.write_all(b"\n"))
+            .expect("write to stdout should succeed");
+    }
 
     Ok(())
 }

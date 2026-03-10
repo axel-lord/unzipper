335b9057d859d5163040bba3bab313bfb3882701
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2026-03-10 15:56:30 +0100

    implemented listing functionality

--- unzipper-lib/src/lib.rs
+++ unzipper-lib/src/lib.rs
@@ -4,6 +4,7 @@ mod encoding;
 
 use ::core::fmt::{self, Debug};
 use ::std::{
+    ffi::OsStr,
     fs::File,
     io::{BufReader, BufWriter, Write},
     path::{Component, Path},
@@ -11,7 +12,7 @@ use ::std::{
 
 use ::chardetng::EncodingDetector;
 use ::tap::Pipe;
-use ::zip::ZipArchive;
+use ::zip::{ZipArchive, read::ZipFile};
 
 pub use self::encoding::{Encoding, EncodingFromStrError};
 
@@ -61,6 +62,9 @@ pub struct Unzipper<'lt> {
     pub encoding: Encoding,
     /// Password to use for files.
     pub password: Option<&'lt [u8]>,
+    /// Null terminate when listing.
+    #[builder(default = false)]
+    pub null_terminate: bool,
 }
 
 impl<'lt> Unzipper<'lt> {
@@ -95,6 +99,25 @@ impl<'lt> Unzipper<'lt> {
         confident.then_some(encoding)
     }
 
+    /// Get encoding either by detection or, by set value.
+    fn get_encoding(&self, archive: &mut Archive, src: &Path) -> &'static ::encoding_rs::Encoding {
+        match self.encoding {
+            Encoding::Auto => self
+                .detect_encoding(archive, src)
+                .inspect(|encoding| {
+                    ::log::info!("using encoding with confidence, {}", encoding.name());
+                })
+                .unwrap_or_else(|| {
+                    ::log::info!("no encoding confidence, using utf-8");
+                    ::encoding_rs::UTF_8
+                }),
+            Encoding::Set(encoding) => {
+                ::log::info!("using set encoding, {}", encoding.name());
+                encoding
+            }
+        }
+    }
+
     /// Attempt to find filename encoding of zip file.
     ///
     /// # Errors
@@ -107,6 +130,42 @@ impl<'lt> Unzipper<'lt> {
             .ok_or(UnzipError::NoEncoding)
     }
 
+    /// List archive contents.
+    ///
+    /// # Errors
+    /// On io errors related to reading src.
+    /// Or if src is not a zip file / is malformed.
+    pub fn list(&self, src: &Path, write: &mut dyn Write) -> Result<(), UnzipError> {
+        let mut archive = self.open_archive(src)?;
+        let encoding = self.get_encoding(&mut archive, src);
+
+        for index in 0..archive.len() {
+            let zip_file = match archive.by_index_raw(index) {
+                Ok(file) => file,
+                Err(err) => {
+                    ::log::warn!("could not get file with index {index} in {src:?}\n{err}");
+                    continue;
+                }
+            };
+
+            let (file_name, _) = encoding.decode_without_bom_handling(zip_file.name_raw());
+
+            // Get any warnings printed as info if verbose.
+            if ::log::log_enabled!(::log::Level::Info) {
+                let path = Path::new(&*file_name);
+                _ = verify_zip_file(&zip_file, path, src, ::log::Level::Info);
+            }
+
+            if let Err(err) = write
+                .write_all(file_name.as_bytes())
+                .and_then(|_| write.write_all(if self.null_terminate { b"\0" } else { b"\n" }))
+            {
+                ::log::error!("write to stdout failed\n{err}")
+            }
+        }
+        Ok(())
+    }
+
     /// Unzip src into dest.
     ///
     /// # Errors
@@ -114,37 +173,84 @@ impl<'lt> Unzipper<'lt> {
     /// Or if src is not a zip file / is malformed.
     /// Or if dest cannot be created / cannot be written to.
     pub fn unzip(&self, src: &Path, dest: Destination<'_>) -> Result<(), UnzipError> {
-        let Self { encoding, password } = self;
-
         let mut archive = self.open_archive(src)?;
 
-        let encoding = match encoding {
-            Encoding::Auto => self
-                .detect_encoding(&mut archive, src)
-                .inspect(|encoding| {
-                    ::log::info!("using encoding with confidence, {}", encoding.name());
-                })
-                .unwrap_or_else(|| {
-                    ::log::info!("no encoding confidence, using utf-8");
-                    ::encoding_rs::UTF_8
-                }),
-            Encoding::Set(encoding) => {
-                ::log::info!("using set encoding, {}", encoding.name());
-                encoding
-            }
-        };
+        let encoding = self.get_encoding(&mut archive, src);
 
         for index in 0..archive.len() {
-            handle_file(&mut archive, index, encoding, src, dest, *password);
+            handle_file(&mut archive, index, encoding, src, dest, self.password);
         }
 
         Ok(())
     }
 }
 
+/// Verification of zip file,
+fn verify_zip_file(
+    zip_file: &ZipFile<BufReader<File>>,
+    path: &Path,
+    src: &Path,
+    level: ::log::Level,
+) -> bool {
+    verify_file_type(zip_file, path, src, level)
+        && verify_path_components(path, src, level)
+        && verify_has_name(path, src, level).is_some()
+}
+
+/// Verify all path components are valid.
+fn verify_path_components(path: &Path, src: &Path, level: ::log::Level) -> bool {
+    for component in path.components() {
+        if let Component::Prefix(_) | Component::RootDir | Component::ParentDir = component {
+            ::log::log!(
+                level,
+                "skipping {path:?} in {src:?}, disallowed path component"
+            );
+            return false;
+        }
+    }
+    true
+}
+
+/// Verify a file is not a symlink.
+fn verify_file_type(
+    zip_file: &ZipFile<BufReader<File>>,
+    path: &Path,
+    src: &Path,
+    level: ::log::Level,
+) -> bool {
+    if zip_file.is_symlink() {
+        ::log::log!(level, "skipping symlink {path:?} in {src:?}, unsupported");
+        false
+    } else {
+        true
+    }
+}
+
+/// Verify a path has a filename.
+fn verify_has_name<'a>(
+    path: &'a Path,
+    src: &Path,
+    level: ::log::Level,
+) -> Option<(&'a Path, &'a OsStr)> {
+    let mut components = path.components();
+    let file_name = loop {
+        match components.next_back() {
+            None => {
+                ::log::log!(level, "path {path:?} in {src:?} has no final component");
+                return None;
+            }
+            Some(Component::Normal(file_name)) => break file_name,
+            Some(_) => {}
+        }
+    };
+    let prefix = components.as_path();
+
+    Some((prefix, file_name))
+}
+
 /// Handle file in archive.
 fn handle_file(
-    archive: &mut ZipArchive<BufReader<File>>,
+    archive: &mut Archive,
     index: usize,
     encoding: &'static ::encoding_rs::Encoding,
     src: &Path,
@@ -166,15 +272,10 @@ fn handle_file(
 
     let (decoded, _has_replaced) = encoding.decode_without_bom_handling(zip_file.name_raw());
     let path = Path::new(&*decoded);
-    for component in path.components() {
-        if let Component::Prefix(_) | Component::RootDir | Component::ParentDir = component {
-            ::log::warn!("skipping {path:?} in {src:?}, disallowed path component");
-            return;
-        }
-    }
 
-    if zip_file.is_symlink() {
-        ::log::warn!("skipping symlink {path:?} in {src:?}, unsupported");
+    if !verify_path_components(path, src, ::log::Level::Warn)
+        | !verify_file_type(&zip_file, path, src, ::log::Level::Warn)
+    {
         return;
     }
 
@@ -199,18 +300,9 @@ fn handle_file(
         return;
     }
 
-    let mut components = path.components();
-    let file_name = loop {
-        match components.next_back() {
-            None => {
-                ::log::warn!("path {path:?} in {src:?} has no final component");
-                return;
-            }
-            Some(Component::Normal(file_name)) => break file_name,
-            Some(_) => {}
-        }
+    let Some((prefix, file_name)) = verify_has_name(path, src, ::log::Level::Warn) else {
+        return;
     };
-    let prefix = components.as_path();
 
     let mut path = exdir.join(prefix);
     if let Err(err) = ::std::fs::create_dir_all(&path) {
--- unzipper/src/main.rs
+++ unzipper/src/main.rs
@@ -90,6 +90,17 @@ struct Cli {
     #[arg(long, requires = "completions", value_enum, default_value_t = default_shell())]
     shell: Shell,
 
+    /// When listing contents terminate with a null character instead of newline.
+    #[arg(
+        long,
+        short = '0',
+        visible_short_alias = 'z',
+        visible_alias = "print0",
+        visible_alias = "null",
+        requires = "list"
+    )]
+    null_terminate: bool,
+
     /// Archive/s to unpack.
     #[arg(group = "action")]
     archive: Vec<PathBuf>,
@@ -103,6 +114,7 @@ fn main() -> ::color_eyre::Result<()> {
         completions,
         detect_encoding,
         shell,
+        null_terminate,
         at,
         list,
         archive,
@@ -141,37 +153,50 @@ fn main() -> ::color_eyre::Result<()> {
         })?;
         writeln!(io::stdout().lock(), "{}", encoding.name())?;
     } else {
-        let unzipper = Unzipper::builder().encoding(encoding).build();
-        let write_to_stdout = |name: &Path| {
-            _ = writeln!(io::stdout().lock(), "{}", name.display());
-        };
-        let unzip = |src| {
-            let result = unzipper.unzip(
-                src,
-                at.as_ref().map_or_else(
-                    || Destination::List(&write_to_stdout),
-                    |dest| Destination::Exdir(dest),
-                ),
-            );
-            if let Err(err) = result {
-                ::log::error!("could not unzip {archive:?}\n{err}");
-            }
-        };
+        let unzipper = Unzipper::builder()
+            .encoding(encoding)
+            .null_terminate(null_terminate)
+            .build();
 
-        match archive.as_slice() {
-            [] => {
-                ::log::warn!("no archives to unzip");
+        if list {
+            for archive in archive {
+                let mut stdout = io::stdout().lock();
+                if let Err(err) = unzipper.list(&archive, &mut stdout) {
+                    ::log::error!("error listing {archive:?}\n{err}");
+                }
             }
-            [archive] => {
-                unzip(archive);
-            }
-            archives => {
-                ThreadPoolBuilder::new()
-                    .thread_name(|idx| format!("unzipper-worker-{idx}"))
-                    .num_threads(threads.get().min(archive.len()))
-                    .build_global()?;
+        } else {
+            let write_to_stdout = |name: &Path| {
+                _ = writeln!(io::stdout().lock(), "{}", name.display());
+            };
+            let unzip = |src| {
+                let result = unzipper.unzip(
+                    src,
+                    at.as_ref().map_or_else(
+                        || Destination::List(&write_to_stdout),
+                        |dest| Destination::Exdir(dest),
+                    ),
+                );
+                if let Err(err) = result {
+                    ::log::error!("could not unzip {archive:?}\n{err}");
+                }
+            };
 
-                archives.par_iter().for_each(|path| unzip(path));
+            match archive.as_slice() {
+                [] => {
+                    ::log::warn!("no archives to unzip");
+                }
+                [archive] => {
+                    unzip(archive);
+                }
+                archives => {
+                    ThreadPoolBuilder::new()
+                        .thread_name(|idx| format!("unzipper-worker-{idx}"))
+                        .num_threads(threads.get().min(archive.len()))
+                        .build_global()?;
+
+                    archives.par_iter().for_each(|path| unzip(path));
+                }
             }
         }
     }
